883c6d608b6dc800bca7601572845fb1022a19a9
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-12 14:19:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |value| 
+  "Hello, my name is #{value}."
+  end
+end
+
+def assign_rooms(array)
+  array.map.with_index do |name, index|
+  "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).map  do |value|
+    puts value
+  end
+  assign_rooms(array).map do |value|
+    puts value
+  end
+end
