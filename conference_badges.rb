78e7f3ea5b14cd9ecdabffc68f0f17ee50cf0613
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-12 11:53:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+return_array = Array.new
+def batch_badge_creator(array)
+  array.each_with_index do { |value| 
+    puts ("Hello, my name is #{value}.")
+    return_array.push("Hello, my name is #{value}.")
+  end
+  }
+end
+  
+  
+def badge_maker(name)
+  return ("Hello, my name is #{name}.")
+end
+
